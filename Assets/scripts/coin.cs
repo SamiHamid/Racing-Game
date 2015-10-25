1f6683bfb5cfca0795dcad734cb07b0ba859909c
SamiHamid <samihamid@hotmail.co.uk>
2015-10-25 18:39:21 +0200

    Project Pushed
    Project Pushed

---
+++ Assets/scripts/coin.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coin : MonoBehaviour {
+
+	private float newXpos;
+	private float newYpos;
+	private float newZpos;
+	private bool falling = true;
+	public Transform explosion;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.forward * Time.deltaTime * 50);
+		if (falling) {
+			transform.Translate(Vector3.forward * Time.deltaTime * 2);
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log("other = " + other);
+		if (other.name == "hilly track"){
+			falling = false;
+		}
+
+		if (other.tag == "barrier"){
+			falling = false;
+		}
+
+		if (other.tag == "Player"){
+			Instantiate(explosion, transform.position, Quaternion.identity);
+			newXpos = Random.Range(10.0f,140.0f);
+			newZpos = Random.Range(5.0f,70.0f);
+			newYpos = 8.0f;
+			transform.position = new Vector3 (newXpos, newYpos, newZpos);
+			falling = true;
+		}
+	}
+	
+}
---
+++ Assets/scripts/manager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class manager : MonoBehaviour {
+	private int lapNumber = 0; 
+	private bool halfPointMet = false; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void increaseLapNumber(){
+		if (halfPointMet) {
+			lapNumber++;
+			halfPointMet = false; 
+		} else {
+			return; 
+		}
+
+		if (lapNumber == 3) {
+			// freeze everything 
+			// Access the rigid body on the car and set isCinematic to true. 
+
+			Application.LoadLevel("winners");
+			Debug.Log("Third lap reached ! "); 
+			lapNumber = 0; 
+		}
+
+		Debug.Log ("Lap Number : " + this.getLapNumber());
+	}
+
+	public int getLapNumber(){
+		return lapNumber; 
+	}
+
+	public void halfLapPassed(){
+		Debug.Log ("HalfLap point passed !");
+		halfPointMet = true; 
+	}
+}
